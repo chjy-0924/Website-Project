1df2ba7a156133dbc78dfc6026f4156627f31c05
Cherry Joy Toribio <ct1270752@gmail.com>
2024-12-06 22:08:34 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Toggle theme
+function toggleTheme() {
+  document.body.classList.toggle("dark-theme");
+}
+
+// Scroll to top
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+// Navbar for mobile
+function myFunction() {
+  const nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
+  } else {
+    nav.className = "topnav";
+  }
+}
+
+// Show "Scroll to Top" button
+window.onscroll = function () {
+  const btn = document.getElementById("scrollToTopBtn");
+  if (document.documentElement.scrollTop > 100) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
